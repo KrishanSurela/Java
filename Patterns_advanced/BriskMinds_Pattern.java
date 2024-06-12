d03c07b819dc3f6ac545bd00e66d7067e0f3b241
KrishanSurela <surelakrishan60@gmail.com>
2024-06-12 18:01:13 +0530

    BriskMinds_Pattern.java

---
+++ Patterns_advanced/BriskMinds_Pattern.java
@@ -0,0 +1,24 @@
+public class BriskMinds_Pattern {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                if ((i == 4 && j == 3) || (i == 4 && j == 2) || (i == 3 && j == 2)) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print(j + " ");
+                }
+            }
+            for (int k = 0; k < 2 * (5 - i) - 1; k++) {
+                System.out.print("  ");
+            }
+            for (int l = (i == 5) ? 2 : 1; l <= i; l++) {
+                if ((i == 4 && l == 3) || (i == 4 && l == 2) || (i == 3 && l == 2)) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print(l + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
